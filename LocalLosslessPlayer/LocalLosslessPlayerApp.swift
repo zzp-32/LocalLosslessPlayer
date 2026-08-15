8e2cf088b68338a7866ec92fadbae8803a6c35fa
zzp-32 <744875773@qq.com>
2026-08-15 20:18:47 +0800

    Improve player responsiveness and add app branding

--- LocalLosslessPlayer/LocalLosslessPlayerApp.swift
+++ LocalLosslessPlayer/LocalLosslessPlayerApp.swift
@@ -1,17 +1,1 @@
-import SwiftUI
-
-@main
-struct LocalLosslessPlayerApp: App {
-    private let persistence = PersistenceController.shared
-    @StateObject private var player = PlayerViewModel()
-    @StateObject private var settings = AppSettings()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistence.container.viewContext)
-                .environmentObject(player)
-                .environmentObject(settings)
-        }
-    }
-}
+base64
